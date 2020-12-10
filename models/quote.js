8ff9b36c09bfd7d53e1db4069d68ce2689540efd
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-12-10 03:01:58 -0500

    fetches quotes from the backend and appends them to the DOM

--- models/quote.js
+++ models/quote.js
@@ -1,18 +1,48 @@
 //Think OOJS - how do you create objects (initialization, classes, instances....SOUNDS LIKE RUBBBBBY)
 
-class Quote{
+class Quote {
     constructor(id, text, author, year, likes) {
         this.id = id;
         this.text = text; 
         this.author = author; 
         this.year = year; 
         this.likes = likes; 
+        this.displayQuote()
+        
     }
+
+
+ quoteHtml(){
+    return `
+        <div class="w3-card w3-margin">
+            <ul class="w3-ul w3-white">
+              <li>
+                <span class="w3-large" id="speech-text">${this.text}</span><br>
+                <span>-${this.author}</span>
+                <p style="font-size:10px">${this.year}</p>
+                <button class="fa fa-thumbs-o-up"> Like</button>
+               <button class="fa fa-arrow-right"> Comment</button>
+                <p>${this.likes} like(s)</p>    
+              </li>
+            </ul>
+          </div>
+          <hr> 
+        `
+    }
+
+    displayQuote(){
+        const quoteContainer =  document.querySelector('#quote-spot');
+        const quoteCard = document.createElement('div')
+        quoteCard.dataset.id = this.id 
+        quoteCard.id = this.id 
+        quoteCard.classList.add = "quote-card"
+        quoteCard.innerHTML += this.quoteHtml()
+        quoteContainer.appendChild(quoteCard)
+    }
+
 }
+
 
 //^^^^^^^^ CONSTRUCTOR METHOD INITIALIZES AND CREATES NEW INSTANCES OF THE QUOTE OBJECT ^^^^^^^^ 
 // Now I need a way to see the object I created (when the user hits submit, the new quote should append to the page) -- Do I do that in this file?   
 
-
-
-    
--- services/API.js
+++ services/API.js
@@ -1,3 +1,50 @@
 //Responsible for initial fetch for data and interaction with the backend 
 
+class API {
+
+    static loadQuotes(){
+        fetch("http://localhost:3000/quotes")
+        .then(response => response.json())
+        .then(quotes => {
+            quotes.forEach(quote => {
+                const{id, text, author, year, likes} = quote 
+                new Quote(id, text, author, year, likes)
+            })
+        })
+    }
+        
+    
+
+    static loadFormListener(){
+        const quoteForm = document.getElementById("quote-form")
+    
+        quoteForm.addEventListener("submit", function(e) {
+            e.preventDefault()
+            const quoteData = formInfo(e)
+            const htmlQuote = quoteHtml(quoteData)
+            attachQuote(htmlQuote)
+            clearForm()
+        })
+    }
+    
+     formInfo(e){
+        return {
+            text: e.target.querySelector('#text-box').value, 
+            author: e.target.querySelector('#author').value,
+            year: e.target.querySelector('#year').value, 
+        }
+     }
+    
+    
+     attachQuote(quote){
+        document.querySelector('#quote-spot').innerHTML += quote
+     }
+    
+     clearForm(){
+        document.querySelector('#text-box').value = "" 
+        document.querySelector('#author').value = ""
+        document.querySelector('#year').value = ""
+     }
+}
+    
  
